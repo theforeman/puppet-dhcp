7ab5f90bfdef6e4378360536e1a829fcba5142d6
Greg Sutcliffe <greg.Sutcliffe@gmail.com>
2012-05-11 23:58:47 +0100

    Test OS properly, tidy up Params file

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,22 +1,21 @@
 class dhcp::params {
 
-    $dhcp_dir = $operatingsystem ? {
-        debian  => "/etc/dhcp",
-        ubuntu  => "/etc/dhcp",
-        darwin  => "/opt/local/etc/dhcp",
-        default => "/etc",
-    }
+  $dhcp_dir = $operatingsystem ? {
+    Ubuntu,debian => "/etc/dhcp",
+    darwin        => "/opt/local/etc/dhcp",
+    default       => "/etc",
+  }
 
-    $packagename = $operatingsystem ? {
-      darwin  => "dhcp",
-      Ubuntu  => "dhcp3-server",
-      default => "isc-dhcp-server",
-    }
+  $packagename = $operatingsystem ? {
+    Ubuntu,Ubuntu => "dhcp3-server",
+    darwin        => "dhcp",
+    default       => "isc-dhcp-server",
+  }
 
-    $servicename = $operatingsystem ? {
-      darwin  => "org.macports.dhcpd",
-      Ubuntu  => "isc-dhcp-server",
-      default => "isc-dhcp-server",
-    }
+  $servicename = $operatingsystem ? {
+    darwin        => "org.macports.dhcpd",
+    Ubuntu,Debian => "isc-dhcp-server",
+    default       => "isc-dhcp-server",
+  }
 
 }
