8edc9aaa11c230dec755c700b08f41df6dbf851a
Greg Sutcliffe <gsutcliffe@ibahn.com>
2012-06-12 22:21:06 +0100

    Use formal regex syntax for default parameters

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,24 +1,24 @@
 class dhcp::params {
 
   $dhcp_dir = $::operatingsystem ? {
-    /redhat,centos,fedora,Scientific/ => '/etc/dhcp',
-    /Ubuntu,Debian/                   => '/etc/dhcp',
-    darwin                            => '/opt/local/etc/dhcp',
-    default                           => '/etc/dhcp',
+    /(redhat|centos|fedora|Scientific)/ => '/etc/dhcp',
+    /(Ubuntu|Debian)/                   => '/etc/dhcp',
+    darwin                              => '/opt/local/etc/dhcp',
+    default                             => '/etc/dhcp',
   }
 
   $packagename = $::operatingsystem ? {
-    /redhat,centos,fedora,Scientific/ => 'dhcp',
-    /Ubuntu,Debian/                   => 'dhcp3-server',
-    darwin                            => 'dhcp',
-    default                           => 'dhcp',
+    /(redhat|centos|fedora|Scientific)/ => 'dhcp',
+    /(Ubuntu|Debian)/                   => 'dhcp3-server',
+    darwin                              => 'dhcp',
+    default                             => 'dhcp',
   }
 
   $servicename = $::operatingsystem ? {
-    /redhat,centos,fedora,Scientific/ => 'dhcpd',
-    /Ubuntu,Debian/                   => 'isc-dhcp-server',
-    darwin                            => 'org.macports.dhcpd',
-    default                           => 'dhcpd',
+    /(redhat|centos|fedora|Scientific)/ => 'dhcpd',
+    /(Ubuntu|Debian)/                   => 'isc-dhcp-server',
+    darwin                              => 'org.macports.dhcpd',
+    default                             => 'dhcpd',
   }
 
 }
