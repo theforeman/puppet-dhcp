2ebfb8ed29b665a3e70e972627380f8d08be8415
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2012-06-29 16:53:44 +0200

    Ensure $dhcp_dir exists

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,6 +37,12 @@ class dhcp (
       }
   }
 
+  file {
+      $dhcp_dir:
+          mode    => '0755',
+          require => Package[$packagename],
+  }
+
   # Only debian and ubuntu have this style of defaults for startup.
   case $::operatingsystem {
     'debian','ubuntu': {
