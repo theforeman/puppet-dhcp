d527648f2b7499ff71e2ea1b7c5af055bca40601
Graeme Gillies <ggillies@redhat.com>
2014-01-31 14:24:18 +1000

    Add the abiltity to specify different pxeservers per dhcp pool

---
+++ spec/defines/pool_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe 'dhcp::pool' do
+  let :title do 'mypool' end
+
+  let :facts do {
+    :osfamily => 'RedHat',
+  } end
+
+  describe 'minimal parameters' do
+    let :params do {
+      :network => '10.0.0.0',
+      :mask    => '255.255.255.0',
+    } end
+
+    it {
+        content = subject.resource('concat_fragment', 'dhcp.conf+70_mypool.dhcp').send(:parameters)[:content]
+        content.split("\n").reject { |c| c =~ /(^\s*#|^$)/ }.should == [
+          "subnet 10.0.0.0 netmask 255.255.255.0 {",
+          "  option subnet-mask 255.255.255.0;",
+          "}",
+        ]
+    }
+  end
+
+  describe 'full parameters' do
+    let :params do {
+      :network   => '10.0.0.0',
+      :mask      => '255.255.255.0',
+      :range     => '10.0.0.10 - 10.0.0.50',
+      :gateway   => '10.0.0.1',
+      :pxeserver => '10.0.0.2',
+    } end
+
+    it {
+        content = subject.resource('concat_fragment', 'dhcp.conf+70_mypool.dhcp').send(:parameters)[:content]
+        content.split("\n").reject { |c| c =~ /(^\s*#|^$)/ }.should == [
+          "subnet 10.0.0.0 netmask 255.255.255.0 {",
+          "  pool",
+          "  {",
+          "    range 10.0.0.10 - 10.0.0.50;",
+          "  }",
+          "  option subnet-mask 255.255.255.0;",
+          "  option routers 10.0.0.1;",
+          "  next-server 10.0.0.2;",
+          "}",
+        ]
+    }
+  end
+end
