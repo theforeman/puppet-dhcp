0bc528dd1ac00bc226d24c8d07adf44294ae1750
Alvin Huang <huang.s.alvin@gmail.com>
2016-07-03 08:31:48 -0400

    add interface-mtu option (#78)

--- spec/defines/pool_spec.rb
+++ spec/defines/pool_spec.rb
@@ -110,6 +110,7 @@ describe 'dhcp::pool' do
       :parameters       => 'max-lease-time 300',
       :nameservers      => ['10.0.0.2', '10.0.0.4'],
       :pxeserver        => '10.0.0.2',
+      :mtu              => 9000,
       :domain_name      => 'example.org',
       :static_routes    => [ { 'mask' => '24', 'network' => '10.0.1.0', 'gateway' => '10.0.0.2' } ],
       :search_domains   => ['example.org', 'other.example.org'],
@@ -132,6 +133,7 @@ describe 'dhcp::pool' do
         "  max-lease-time 300;",
         "  option domain-name-servers 10.0.0.2, 10.0.0.4;",
         "  option domain-search \"example.org\", \"other.example.org\";",
+        "  option interface-mtu 9000;",
         "  next-server 10.0.0.2;",
         "}",
       ])
