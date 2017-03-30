390df0eb40e1943c7e37b385bf5c4b53b73d129f
Daniel Schaaff <dschaaff@knuedge.com>
2017-03-30 16:55:55 +0200

    Add $ddns_updates variable
    Closes GH-69

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -133,6 +133,44 @@ describe 'dhcp' do
           ])
         }
       end
+
+      describe "ddns-updates without key" do
+        let(:params) do {
+          :interfaces => ['eth0'],
+          :ddns_updates => true,
+        } end
+
+        let(:facts) do
+          facts.merge({:domain => 'example.org'})
+        end
+
+        it { should compile.with_all_deps }
+
+        it {
+          verify_concat_fragment_exact_contents(catalogue, 'dhcp.conf+01_main.dhcp', [
+            'omapi-port 7911;',
+            'default-lease-time 43200;',
+            'max-lease-time 86400;',
+            'ddns-updates on;',
+            'ddns-update-style interim;',
+            'update-static-leases on;',
+            'use-host-decl-names on;',
+            'zone example.org. {',
+            '  primary 8.8.8.8;',
+            '}',
+            'option domain-name "example.org";',
+            'option domain-name-servers 8.8.8.8, 8.8.4.4;',
+            "option ntp-servers none;",
+            'allow booting;',
+            'allow bootp;',
+            'option fqdn.no-client-update    on;  # set the "O" and "S" flag bits',
+            'option fqdn.rcode2            255;',
+            'option pxegrub code 150 = text ;',
+            'log-facility local7;',
+            "include \"#{conf_path}/dhcpd.hosts\";",
+          ])
+        }
+      end
     end
   end
 end
