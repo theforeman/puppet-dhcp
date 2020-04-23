805f4f77f9416dae54c17cfcedbc779be7c9b3a9
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-23 14:59:04 +0200

    Clean up init spec
    This uses inheritance to reduce duplication

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,22 +7,22 @@ describe 'dhcp' do
 
       let(:params) do
         {
-          :interfaces => ['eth0'],
-        }.merge(overridden_params)
+          interfaces: ['eth0'],
+        }
       end
 
-      conf_path = case os
-                  when /^FreeBSD/i
-                    '/usr/local/etc'
-                  when /^Archlinux/i
-                    '/etc'
-                  else
-                    '/etc/dhcp'
-                  end
-      describe "dhcp class without any parameters on #{os}" do
-        let(:overridden_params) do {
-        } end
+      let(:conf_path) do
+        case os
+        when /^FreeBSD/i
+          '/usr/local/etc'
+        when /^Archlinux/i
+          '/etc'
+        else
+          '/etc/dhcp'
+        end
+      end
 
+      describe "without any parameters" do
         it { should compile.with_all_deps }
 
         it {
@@ -72,30 +72,32 @@ describe 'dhcp' do
         end
       end
 
-      describe "dhcp class parameters on #{os}" do
-        let(:overridden_params) do {
-          :dnsupdatekey  => 'mydnsupdatekey',
-          :ntpservers    => ['1.1.1.1', '1.1.1.2'],
-          :omapi_name    => 'mykeyname',
-          :omapi_key     => 'myomapikey',
-          :pxeserver     => '10.0.0.5',
-          :mtu           => 9000,
-          :pxefilename   => 'mypxefilename',
-          :ipxe_filename => 'myipxefilename',
-          :bootfiles     => {
-            '00:00'       => 'pxelinux.0',
-            '00:06'       => 'shim.efi',
-            '00:07'       => 'shim.efi',
-            '00:09'       => 'shim.efi',
-          },
-          :option_static_route => true,
-          :options      => ['provision-url code 224 = text', 'provision-type code 225 = text'],
-          :authoritative => true,
-          :ddns_domainname => 'example.com',
-          :ddns_rev_domainname => 'in-addr.arpa',
-          :ddns_update_style => 'standard',
-          :includes => ['myinclude1', 'myinclude2'],
-        } end
+      describe "with all parameters" do
+        let(:params) do
+          super().merge(
+            :dnsupdatekey  => 'mydnsupdatekey',
+            :ntpservers    => ['1.1.1.1', '1.1.1.2'],
+            :omapi_name    => 'mykeyname',
+            :omapi_key     => 'myomapikey',
+            :pxeserver     => '10.0.0.5',
+            :mtu           => 9000,
+            :pxefilename   => 'mypxefilename',
+            :ipxe_filename => 'myipxefilename',
+            :bootfiles     => {
+              '00:00'       => 'pxelinux.0',
+              '00:06'       => 'shim.efi',
+              '00:07'       => 'shim.efi',
+              '00:09'       => 'shim.efi',
+            },
+            :option_static_route => true,
+            :options      => ['provision-url code 224 = text', 'provision-type code 225 = text'],
+            :authoritative => true,
+            :ddns_domainname => 'example.com',
+            :ddns_rev_domainname => 'in-addr.arpa',
+            :ddns_update_style => 'standard',
+            :includes => ['myinclude1', 'myinclude2'],
+          )
+        end
 
         it { should compile.with_all_deps }
 
@@ -158,18 +160,16 @@ describe 'dhcp' do
           ])
         }
 
-          it {
-            verify_concat_fragment_exact_contents(catalogue, 'dhcp.conf+20_includes', [
-              'include "myinclude1";',
-              'include "myinclude2";',
-            ])
-          }
+        it {
+          verify_concat_fragment_exact_contents(catalogue, 'dhcp.conf+20_includes', [
+            'include "myinclude1";',
+            'include "myinclude2";',
+          ])
+        }
       end
 
-      describe "ddns-updates without key" do
-        let(:overridden_params) do {
-          :ddns_updates => true,
-        } end
+      describe "with ddns-updates without key" do
+        let(:params) { super().merge(ddns_updates: true) }
 
         it { should compile.with_all_deps }
 
@@ -201,9 +201,7 @@ describe 'dhcp' do
       end
 
       describe "without omapi" do
-        let(:overridden_params) do {
-          :omapi => false,
-        } end
+        let(:params) { super().merge(omapi: false) }
 
         it { should compile.with_all_deps }
 
@@ -228,121 +226,93 @@ describe 'dhcp' do
       end
 
       describe "without bootp" do
-        let(:overridden_params) do {
-          :bootp => false,
-        } end
+        let(:params) { super().merge(bootp: false) }
 
         it { should compile.with_all_deps }
-
-        it {
-          verify_concat_fragment_exact_contents(catalogue, 'dhcp.conf+01_main.dhcp', [
-            'omapi-port 7911;',
-            'default-lease-time 43200;',
-            'max-lease-time 86400;',
-            'not authoritative;',
-            'ddns-update-style none;',
-            'option domain-name "example.com";',
-            'option domain-name-servers 8.8.8.8, 8.8.4.4;',
-            "option ntp-servers none;",
-            'allow booting;',
-            'option fqdn.no-client-update    on;  # set the "O" and "S" flag bits',
-            'option fqdn.rcode2            255;',
-            'option pxegrub code 150 = text ;',
-            'log-facility local7;',
-            "include \"#{conf_path}/dhcpd.hosts\";",
-          ])
-        }
+        it { should contain_concat__fragment('dhcp.conf+01_main.dhcp').without_content(/allow bootp/) }
       end
 
-      describe "with failover, bootp undef" do
-        let(:overridden_params) do {
-          :failover => true
-        } end
+      describe 'with failover' do
+        let(:params) { super().merge(failover: true) }
 
-        it { should compile.with_all_deps }
+        describe "and bootp undef" do
+          it { should compile.with_all_deps }
 
-        it {
-          verify_concat_fragment_exact_contents(catalogue, 'dhcp.conf+01_main.dhcp', [
-            'omapi-port 7911;',
-            'default-lease-time 43200;',
-            'max-lease-time 86400;',
-            'not authoritative;',
-            'ddns-update-style none;',
-            'option domain-name "example.com";',
-            'option domain-name-servers 8.8.8.8, 8.8.4.4;',
-            "option ntp-servers none;",
-            'allow booting;',
-            'option fqdn.no-client-update    on;  # set the "O" and "S" flag bits',
-            'option fqdn.rcode2            255;',
-            'option pxegrub code 150 = text ;',
-            'log-facility local7;',
-            "include \"#{conf_path}/dhcpd.hosts\";",
-          ])
-        }
-      end
+          it {
+            verify_concat_fragment_exact_contents(catalogue, 'dhcp.conf+01_main.dhcp', [
+              'omapi-port 7911;',
+              'default-lease-time 43200;',
+              'max-lease-time 86400;',
+              'not authoritative;',
+              'ddns-update-style none;',
+              'option domain-name "example.com";',
+              'option domain-name-servers 8.8.8.8, 8.8.4.4;',
+              "option ntp-servers none;",
+              'allow booting;',
+              'option fqdn.no-client-update    on;  # set the "O" and "S" flag bits',
+              'option fqdn.rcode2            255;',
+              'option pxegrub code 150 = text ;',
+              'log-facility local7;',
+              "include \"#{conf_path}/dhcpd.hosts\";",
+            ])
+          }
+        end
 
-      describe "with failover, bootp true" do
-        let(:overridden_params) do {
-          :failover => true,
-          :bootp => true
-        } end
+        describe "and bootp true" do
+          let(:params) { super().merge(bootp: true) }
 
-        it { should compile.with_all_deps }
+          it { should compile.with_all_deps }
 
-        it {
-          verify_concat_fragment_exact_contents(catalogue, 'dhcp.conf+01_main.dhcp', [
-            'omapi-port 7911;',
-            'default-lease-time 43200;',
-            'max-lease-time 86400;',
-            'not authoritative;',
-            'ddns-update-style none;',
-            'option domain-name "example.com";',
-            'option domain-name-servers 8.8.8.8, 8.8.4.4;',
-            "option ntp-servers none;",
-            'allow booting;',
-            'allow bootp;',
-            'option fqdn.no-client-update    on;  # set the "O" and "S" flag bits',
-            'option fqdn.rcode2            255;',
-            'option pxegrub code 150 = text ;',
-            'log-facility local7;',
-            "include \"#{conf_path}/dhcpd.hosts\";",
-          ])
-        }
-      end
+          it {
+            verify_concat_fragment_exact_contents(catalogue, 'dhcp.conf+01_main.dhcp', [
+              'omapi-port 7911;',
+              'default-lease-time 43200;',
+              'max-lease-time 86400;',
+              'not authoritative;',
+              'ddns-update-style none;',
+              'option domain-name "example.com";',
+              'option domain-name-servers 8.8.8.8, 8.8.4.4;',
+              "option ntp-servers none;",
+              'allow booting;',
+              'allow bootp;',
+              'option fqdn.no-client-update    on;  # set the "O" and "S" flag bits',
+              'option fqdn.rcode2            255;',
+              'option pxegrub code 150 = text ;',
+              'log-facility local7;',
+              "include \"#{conf_path}/dhcpd.hosts\";",
+            ])
+          }
+        end
 
-      describe "with failover, bootp false" do
-        let(:overridden_params) do {
-          :failover => true,
-          :bootp => false
-        } end
+        describe "and bootp false" do
+          let(:params) { super().merge(bootp: false) }
 
-        it { should compile.with_all_deps }
+          it { should compile.with_all_deps }
 
-        it {
-          verify_concat_fragment_exact_contents(catalogue, 'dhcp.conf+01_main.dhcp', [
-            'omapi-port 7911;',
-            'default-lease-time 43200;',
-            'max-lease-time 86400;',
-            'not authoritative;',
-            'ddns-update-style none;',
-            'option domain-name "example.com";',
-            'option domain-name-servers 8.8.8.8, 8.8.4.4;',
-            "option ntp-servers none;",
-            'allow booting;',
-            'option fqdn.no-client-update    on;  # set the "O" and "S" flag bits',
-            'option fqdn.rcode2            255;',
-            'option pxegrub code 150 = text ;',
-            'log-facility local7;',
-            "include \"#{conf_path}/dhcpd.hosts\";",
-          ])
-        }
+          it {
+            verify_concat_fragment_exact_contents(catalogue, 'dhcp.conf+01_main.dhcp', [
+              'omapi-port 7911;',
+              'default-lease-time 43200;',
+              'max-lease-time 86400;',
+              'not authoritative;',
+              'ddns-update-style none;',
+              'option domain-name "example.com";',
+              'option domain-name-servers 8.8.8.8, 8.8.4.4;',
+              "option ntp-servers none;",
+              'allow booting;',
+              'option fqdn.no-client-update    on;  # set the "O" and "S" flag bits',
+              'option fqdn.rcode2            255;',
+              'option pxegrub code 150 = text ;',
+              'log-facility local7;',
+              "include \"#{conf_path}/dhcpd.hosts\";",
+            ])
+          }
+        end
       end
 
       describe "with config_comment" do
         context 'with multiple lines' do
-          let(:overridden_params) do {
-            :config_comment => "first line\nsecond line"
-          } end
+          let(:params) { super().merge(config_comment: "first line\nsecond line") }
 
           it do
             verify_concat_fragment_contents(catalogue, 'dhcp.conf+01_main.dhcp', [
