e0abfe1e390e0b789e6bca1c87c9edac8082be4b
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-05-21 19:04:42 +0200

    Switch to voxpupuli-acceptance-1.0

--- spec/acceptance/include_spec.rb
+++ spec/acceptance/include_spec.rb
@@ -3,26 +3,27 @@ require 'spec_helper_acceptance'
 describe 'Installation with include statement' do
   interface = 'eth0'
 
-  let(:pp) do
-    <<-EOS
-    $interface = $facts['networking']['interfaces']['#{interface}']
+  it_behaves_like 'an idempotent resource' do
+    let(:manifest) do
+      <<-EOS
+      $interface = $facts['networking']['interfaces']['#{interface}']
 
-    file { '/etc/dhcp.include':
-      ensure => file,
-    }
+      file { '/etc/dhcp.include':
+        ensure => file,
+      }
 
-    class { 'dhcp':
-      interfaces => ['#{interface}'],
-      includes   => '/etc/dhcp.include',
-    }
+      class { 'dhcp':
+        interfaces => ['#{interface}'],
+        includes   => '/etc/dhcp.include',
+      }
 
-    dhcp::pool { "default subnet":
-      network => $interface['network'],
-      mask    => $interface['netmask'],
-    }
-    EOS
+      dhcp::pool { "default subnet":
+        network => $interface['network'],
+        mask    => $interface['netmask'],
+      }
+      EOS
+    end
   end
 
-  it_behaves_like 'a idempotent resource'
   it_behaves_like 'a DHCP server'
 end
