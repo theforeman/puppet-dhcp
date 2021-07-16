1e17866d401a1376b4a7e4fec8362812dd008329
Joshua Hoblitt <josh@hoblitt.com>
2021-07-16 09:36:38 -0700

    fix dhcp::host comment param

--- spec/defines/host_spec.rb
+++ spec/defines/host_spec.rb
@@ -2,27 +2,35 @@ require 'spec_helper'
 
 describe 'dhcp::host' do
   on_supported_os.each do |os, facts|
+    let :title do 'myhost' end
+
+    let :params do {
+      :ip  => '10.0.0.100',
+      :mac => '01:02:03:04:05:06',
+    } end
+
+    let :facts do
+      facts
+    end
+
+    let :pre_condition do
+      "class { '::dhcp': interfaces => ['eth0']}"
+    end
+
+    let(:fragment_name) do
+      'dhcp.hosts+10_myhost.hosts'
+    end
+
+    shared_examples "a concat template" do
+      it { is_expected.to compile.with_all_deps }
+
+      it do
+        is_expected.to contain_concat__fragment(fragment_name).with_content(expected_content)
+      end
+    end
+
     context "on #{os}" do
-      let :title do 'myhost' end
-
       describe 'minimal parameters' do
-        let :params do {
-          :ip  => '10.0.0.100',
-          :mac => '01:02:03:04:05:06',
-        } end
-
-        let :facts do
-          facts
-        end
-
-        let :pre_condition do
-          "class { '::dhcp': interfaces => ['eth0']}"
-        end
-
-        let(:fragment_name) do
-          'dhcp.hosts+10_myhost.hosts'
-        end
-
         let(:expected_content) do
           <<~CONTENT
             host myhost {
@@ -33,9 +41,24 @@ describe 'dhcp::host' do
           CONTENT
         end
 
-        it do
-          is_expected.to contain_concat__fragment(fragment_name).with_content(expected_content)
+        it_behaves_like "a concat template"
+      end
+
+      describe 'comment parameter' do
+        let(:params) { super().merge(comment: 'a useful comment') }
+
+        let(:expected_content) do
+          <<~CONTENT
+            # a useful comment
+            host myhost {
+              hardware ethernet   01:02:03:04:05:06;
+              fixed-address       10.0.0.100;
+              ddns-hostname       "myhost";
+            }
+          CONTENT
         end
+
+        it_behaves_like "a concat template"
       end
     end
   end
