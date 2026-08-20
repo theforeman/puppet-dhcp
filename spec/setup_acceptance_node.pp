b40a7b0892481c1740f7c4325efff3c413e6f51c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2026-08-20 11:11:53 +0200

    Add EL 10 support

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -1,4 +1,5 @@
-if $facts['os']['family'] == 'RedHat' and $facts['os']['name'] != 'Fedora' {
+# ISC DHCP is shipped in EPEL
+if $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] == '10' {
   package { 'epel-release':
     ensure => installed,
   }
